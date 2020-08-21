912417450a9f5904ebe1494bcfaca968c2bb4f4e
Samoljak <DSamoljak@tobit.ag>
2020-08-21 17:26:42 +0200

    :construction: filtering the sites by searching with input.

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ function init() {
     const button = document.querySelector('.btn');
     button.addEventListener('click', sendtoIntercom);
 
+    let timeout = null;
     const textarea = document.getElementById('search');
-    textarea.addEventListener('input', searchSite);
-}
+    textarea.addEventListener('input', () => {
+        if (timeout !== null) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            searchSite();
+        }, 1000);
+    });
 
-// List of the sites.
-// fetch.
-fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
-.then(response => response.json())
-.then(data => buildElements(data.Data));
+    searchSite();
+}
 
 // Creating the Elements
 function buildElements(Data) {
@@ -25,22 +29,31 @@ function buildElements(Data) {
     Data.forEach((site) => {
         const div = document.createElement('div');
         const image = document.createElement('img');
-        const p = document.createTextNode(site.appstoreName);
+        const p = document.createElement('div');
 
         // divs
         div.className = 'list__items';
 
-        // images
-        image.src = `https://chayns.tobit.com/storage/${site.siteId}/Images/icon-57.png`;
-        image.className = 'site__img';
+        fetch(`https://chayns.tobit.com/storage/${site.siteId}/Images/icon-57.png`)
+            .then(() => {
+                image.src = `https://chayns.tobit.com/storage/${site.siteId}/Images/icon-57.png`;
+            })
+            .catch(() => {
+                image.src = `https://chayns.tobit.com/storage/77892-13928/Images/icon-57.png`;
+            });
+
+            
         image.onclick = function openSite() { 
             window.open(`https://chayns.net/${site.siteId}/`);
         };
+        // images
+        image.className = 'site__img';
+        p.innerHTML = site.appstoreName;
+        p.className = 'site__name';
 
         list.appendChild(div);
-
-        // textnodes
         div.appendChild(image);
+        // textnodes
         div.appendChild(p);
     });
 }
@@ -60,9 +73,14 @@ function sendtoIntercom() {
 }
 // Search for an site.
 function searchSite() {
-    const searchedSite = document.querySelector('search'); 
-    chayns.findSite(searchedSite, 0, 5)
-    .then(console.log);
+    const searchedSite = document.querySelector('#search');
+    // chayns.findSite(searchedSite.value)
+    // .then((data) => buildElements(data.Value));
+    // List of the sites.
+    // fetch.
+    fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=${searchedSite.value}&Skip=0&Take=50`)
+    .then(response => response.json())
+    .then(data => buildElements(data.Data));
 }
 
 init();
