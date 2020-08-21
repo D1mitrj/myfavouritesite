b4d32c800e3f688e56a8616cc14b3b342b91b076
Samoljak <DSamoljak@tobit.ag>
2020-08-21 12:12:52 +0200

    :hammer: Fetching data from JSON TappApi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import login from './utils/login';
  import './app.scss';
+// import { data } from 'autoprefixer';
 
 // const handleUserIsLoggedIn = ($introElement, $loginBtn) => {
 //     $introElement.innerText = `Hallo ${chayns.env.user.firstName}`;
@@ -38,4 +39,41 @@
 // init();
 
 // accordion dropdown
-// chayns.ui.initAll();
+chayns.ui.initAll();
+
+// List of the sites.
+// fetch.
+fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
+.then(response => response.json())
+//   .then(data => console.log(data));
+.then(data => buildElements(data.Data));
+
+// Creating the Elements
+function buildElements(Data) {
+    Data.forEach((site) => {
+        const image = document.createElement('img');
+        const div = document.createElement('div');
+        const p = document.createTextNode(site.appstoreName);
+
+        // divs
+        div.className = 'list__items';
+
+        // images
+        image.src = `https://chayns.tobit.com/storage/${site.siteId}/Images/icon-57.png`;
+        image.className = 'site__img';
+        image.onclick = function openSite() { 
+            window.open(`https://chayns.net/${site.siteId}/`);
+        };
+
+        const list = document.querySelector('.list');
+        list.appendChild(div);
+
+        // textnodes
+        div.appendChild(image);
+        div.appendChild(p);
+    });
+}
+// Formulardata send via intercom.
+function sendtoIntercom () {
+    let name = doc
+}
