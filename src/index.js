ab5ca094fefb1d9b144226c1b92b2895a07573e4
Samoljak <DSamoljak@tobit.ag>
2020-08-21 15:45:36 +0200

    :hammer: sending formular to page.

--- src/index.js
+++ src/index.js
@@ -1,58 +1,30 @@
-// import login from './utils/login';
- import './app.scss';
-// import { data } from 'autoprefixer';
-
-// const handleUserIsLoggedIn = ($introElement, $loginBtn) => {
-//     $introElement.innerText = `Hallo ${chayns.env.user.firstName}`;
-//     $loginBtn.innerText = 'Abmelden';
-//     $loginBtn.addEventListener('click', async () => {
-//         await chayns.logout();
-//         window.location.reload();
-//     });
-// };
-
-// const handleUserIsLoggedOut = ($introElement, $loginBtn) => {
-//     $introElement.innerText = 'Bitte melde dich an';
-//     $loginBtn.innerText = 'Anmelden';
-//     $loginBtn.addEventListener('click', () => {
-//         login(() => handleUserIsLoggedIn($introElement, $loginBtn));
-//     });
-// };
-
-// const init = async () => {
-//     try {
-//         await chayns.ready;
-
-//         const $introElement = document.querySelector('#intro');
-//         const $loginBtn = document.querySelector('#loginBtn');
-
-//         if (chayns.env.user.isAuthenticated) {
-//             handleUserIsLoggedIn($introElement, $loginBtn);
-//         } else {
-//             handleUserIsLoggedOut($introElement, $loginBtn);
-//         }
-//     } catch (err) {
-//         console.error('No chayns environment found', err);
-//     }
-// };
-
-// init();
+import './app.scss';
 
 // accordion dropdown
 chayns.ui.initAll();
+
+function init() {
+    const button = document.querySelector('.btn');
+    button.addEventListener('click', sendtoIntercom);
+
+    const textarea = document.getElementById('search');
+    textarea.addEventListener('input', searchSite);
+}
 
 // List of the sites.
 // fetch.
 fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
 .then(response => response.json())
-//   .then(data => console.log(data));
 .then(data => buildElements(data.Data));
 
 // Creating the Elements
 function buildElements(Data) {
+    const list = document.querySelector('.list');
+    list.innerHTML = '';
+
     Data.forEach((site) => {
-        const image = document.createElement('img');
         const div = document.createElement('div');
+        const image = document.createElement('img');
         const p = document.createTextNode(site.appstoreName);
 
         // divs
@@ -65,7 +37,6 @@ function buildElements(Data) {
             window.open(`https://chayns.net/${site.siteId}/`);
         };
 
-        const list = document.querySelector('.list');
         list.appendChild(div);
 
         // textnodes
@@ -74,6 +45,24 @@ function buildElements(Data) {
     });
 }
 // Formulardata send via intercom.
-function sendtoIntercom () {
-    let name = doc
+function sendtoIntercom() {
+    const name = document.querySelector('.name').value;
+    const email = document.querySelector('.e_mail').value;
+    const adresse = document.querySelector('.adresse').value;
+    const kommentar = document.querySelector('.kommentar').value;
+    const linkderseite = document.querySelector('.link_der_seite').value;
+
+    chayns.intercom.sendMessageToPage({ 
+       text: `Name: ${name} E-Mail: ${email} Adresse: ${adresse} Kommentar: ${kommentar} Link der Seite: ${linkderseite}`
+    }).then((data) => {
+        if (data.status === 200)chayns.dialog.alert('', 'thank you');
+    });
 }
+// Search for an site.
+function searchSite() {
+    const searchedSite = document.querySelector('search'); 
+    chayns.findSite(searchedSite, 0, 5)
+    .then(console.log);
+}
+
+init();
